f54801e7627dc41f3d71eeda0d6c2bc7963bc3c4
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-30 18:32:26 -0400

    images

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,28 +3,32 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 //import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('World\'s Greatest Password Manager'),
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-        ),
-        body: Center(
-          child: Text(
-            'You\'re gonna love this',
-            style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                fontFamily: 'Mono',
-                color: Colors.green[600]),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-          backgroundColor: Colors.blue,
-        ),
+void main() => runApp(MaterialApp(home: Home()));
+
+//home screen
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('World\'s Greatest Password Manager'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
       ),
-    ));
+      body: Center(
+          child: Image(
+        image: AssetImage('img/photo-1603899122634-f086ca5f5ddd.jpeg'),
+
+        // image: NetworkImage(
+        //     'https://images.unsplash.com/photo-1603899122634-f086ca5f5ddd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80'),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click me'),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}

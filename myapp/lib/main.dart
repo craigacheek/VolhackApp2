1aed6c8dac6ad57b0b259c40fdcfb9ca26b6a0b0
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-30 12:05:24 -0400

    updates

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('World\'s Greatest Password Manager'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('You\'re gonna love this')),
-  ),
-
-));
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('World\'s Greatest Password Manager'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('You\'re gonna love this'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+        ),
+      ),
+    ));

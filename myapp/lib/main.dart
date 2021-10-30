f0ada71aa0f3b58e112dfb0a4d73a406cb88c6a9
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-30 00:20:29 -0400

    update title

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'We Forget Passwords For You'),
+      home: const MyHomePage(title: 'The greatest password manager in the world'),
     );
   }
 }

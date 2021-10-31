cddf741023ef65d30e04f8474368b9bf6935297f
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-30 20:02:54 -0400

    Homescreen styling and button

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,34 +1,42 @@
+import 'dart:async';
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-//import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
 
-//home screen
+//build home screen
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('World\'s Greatest Password Manager'),
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-      ),
-      body: Center(
-          child: Image(
-        image: AssetImage('img/photo-1603899122634-f086ca5f5ddd.jpeg'),
-
-        // image: NetworkImage(
-        //     'https://images.unsplash.com/photo-1603899122634-f086ca5f5ddd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80'),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click me'),
-        backgroundColor: Colors.blue,
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            'World\'s Greatest Password Manager',
+            style: TextStyle(fontFamily: 'Mono'),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        //Homescreen Image
+        body: Center(
+          child: ElevatedButton.icon(
+              //styling for button
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              onPressed: () {
+                print("test");
+              },
+              //styling for button icon
+              icon: const Icon(
+                Icons.lock_open,
+                color: Colors.purple,
+              ),
+              //button text
+              label: Text('Let\'s Get Started!')),
+        ));
   }
 }

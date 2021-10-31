d93a3f39e55744e1ac282059b58bef62b55f42ce
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-30 22:33:58 -0400

    new view

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -12,31 +12,73 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        //header bar properties
+        appBar: AppBar(
+          title: Text(
+            'World\'s Most Reliable Password Manager',
+            style: TextStyle(fontFamily: 'Mono'),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple[300],
+        ),
+        //Homescreen Image
+        body: Container(
+          //body container properties
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/vault.jpeg'), fit: BoxFit.cover)),
+          //button properties
+          child: Container(
+              alignment: Alignment.center,
+              child: ElevatedButton.icon(
+                  //styling for button
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple[300],
+                      fixedSize: const Size(500, 150),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => List()));
+                  },
+                  //styling for button icon
+                  icon: const Icon(
+                    Icons.lock_open,
+                    color: Colors.white,
+                    size: 70,
+                  ),
+                  //button text
+                  label: Text(
+                    'Let\'s Get Started!',
+                    style: TextStyle(fontSize: 50),
+                  ))),
+        ));
+  }
+}
+
+//build List screen
+class List extends StatelessWidget {
+  const List({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //header bar properties
         appBar: AppBar(
           title: Text(
             'World\'s Greatest Password Manager',
             style: TextStyle(fontFamily: 'Mono'),
           ),
           centerTitle: true,
-          backgroundColor: Colors.purple,
+          backgroundColor: Colors.deepPurple[300],
         ),
         //Homescreen Image
-        body: Center(
-          child: ElevatedButton.icon(
-              //styling for button
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-              ),
-              onPressed: () {
-                print("test");
-              },
-              //styling for button icon
-              icon: const Icon(
-                Icons.lock_open,
-                color: Colors.purple,
-              ),
-              //button text
-              label: Text('Let\'s Get Started!')),
+        body: Container(
+          //body container properties
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/lock.jpeg'), fit: BoxFit.cover)),
         ));
   }
 }
